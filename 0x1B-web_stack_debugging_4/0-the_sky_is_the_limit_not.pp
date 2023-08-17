211303475af02ac13632eb8caf0d07b492723ca7
Angie <angie.monnye@gmail.com>
2023-08-17 15:58:46 +0200

    Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,12 @@
-# increasing the ulimit
+# increases the amount of traffic an Nginx server can handle.
 
 file { '/etc/default/nginx':
   ensure  => file,
-  content => "ULIMIT='-n 2048'\n",
+  content => "ULIMIT='-n 4096'\n",
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/default/nginx'],
 }
